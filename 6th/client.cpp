830a0da2cb00b99bb12f7298a0f63f7c0253f655
Hsue66 <dufmaaked@gmail.com>
2015-10-09 07:02:10 +0900

    comment

--- 6th/client.cpp
+++ 6th/client.cpp
@@ -14,35 +14,44 @@
 #define BUFFER_SIZE 100
 
 
-int main(int argc, char** argv) {
-	if (argc != 2) {
+int main(int argc, char** argv) 
+{
+	if (argc != 2) 
+	{
 		printf("Usage: %s IPv4-address\n", argv[0]);
 		return -1;
 	}
+
 	struct sockaddr_in connectSocket;
 	memset(&connectSocket, 0, sizeof(connectSocket));
 	connectSocket.sin_family = AF_INET;
 	inet_aton(argv[1], (struct in_addr*) &connectSocket.sin_addr.s_addr);
 	connectSocket.sin_port = htons(PORT);
+	
 	int connectFD = socket(AF_INET, SOCK_STREAM, 0);
-	if (connect(connectFD, (struct sockaddr*) &connectSocket, sizeof(connectSocket)) == -1) {
+
+	if (connect(connectFD, (struct sockaddr*) &connectSocket, sizeof(connectSocket)) == -1)
+	{
 		printf("Can not connect.\n");
 		return -1;
-	} else {
+	} 
+	else 
+	{
 		int readBytes, writtenBytes;
 		char sendBuffer[BUFFER_SIZE];
 		char receiveBuffer[BUFFER_SIZE];
-		while (true) {
-			//서버에 문자열을 보낸 뒤 서버가 보낸 echo를 받아 출력하시오.
-			 fgets(sendBuffer,sizeof(sendBuffer),stdin);
-			 
+		while (true) 
+		{
+			// send string to server and receive string from server
+			 scanf("%s",sendBuffer);
 			 write(connectFD,sendBuffer, strlen(sendBuffer));
-
+			
 			 writtenBytes = read(connectFD,receiveBuffer, BUFFER_SIZE-1);
 			 receiveBuffer[writtenBytes]='\0';
-			printf("%s",receiveBuffer);
+			 printf("%s\n",receiveBuffer);
 		}
 	}
 	close(connectFD);
+
 	return 0;
 }
