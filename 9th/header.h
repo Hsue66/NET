fcd03f179ecd1f92d1ae46982b9999da7fc0d07c
Hsue66 <dufmaakedl@gmail.com>
2015-11-06 21:45:09 +0900

    edit

--- 9th/header.h
+++ 9th/header.h
@@ -1,18 +1,13 @@
-#include <stdio.h>
+# include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
-# include <netinet/in.h>
-# include <sys/socket.h>
-# include <arpa/inet.h>
-# include <unistd.h>
 # include <errno.h>
-# include <sys/types.h>
-# include <wait.h>
-#include <semaphore.h>
-#include <pthread.h>
+# include <semaphore.h>
+# include <pthread.h>
+
 void unix_error(const char *msg)
-{
-	    int errnum = errno;
-		    fprintf(stderr, "%s (%d: %s)\n", msg, errnum, strerror(errnum));
-			    exit(EXIT_FAILURE);
+{    
+	int errnum = errno;	    
+	fprintf(stderr, "%s (%d: %s)\n", msg, errnum, strerror(errnum));	
+	exit(EXIT_FAILURE);
 }
